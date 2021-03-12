9d33ea5226e6fd3b9e8bce099708d888c48b27f6
blue@github <blue@github>
2021-03-12 19:02:46 +0800

    new version

--- python/examples/yolov3_coco.py
+++ python/examples/yolov3_coco.py
@@ -86,33 +86,38 @@ class_names = ["person",
     "hair drier",
     "toothbrush"]
 
-image = cv2.imread(r'C:\Users\DY\Cache\cv_test_video\d5.png', 1)
+image = cv2.imread(r'', 1)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image_resized = cv2.resize(image_rgb, (416, 416), interpolation=cv2.INTER_LINEAR)
 data = ncnn.Mat.from_pixels(image_resized.data, ncnn.Mat.PixelType.PIXEL_RGB, 416, 416)
 #data = ncnn.Mat.from_pixels_resize(image.data, ncnn.Mat.PixelType.PIXEL_BGR2RGB, 1280, 720, 416, 416)
-data.substract_mean_normalize([0.0,0.0,0.0],[1/255.0,1/255.0,1/255.0])
+
+#data.substract_mean_normalize([0.0,0.0,0.0],[1/255.0,1/255.0,1/255.0])
 
 net = ncnn.Net()
 net.opt.use_vulkan_compute = False
-ret = net.load_param(r'C:\Users\DY\Cache\ncnn\yolov3.param')
+ret = net.load_param(r'')
 if ret != 0:
     print('Failed to load param file')
 
-ret = net.load_model(r'C:\Users\DY\Cache\ncnn\yolov3.bin')
+ret = net.load_model(r'')
 if ret != 0:
     print('Failed to load bin file.')
 
 start = time.time()
 ex = net.create_extractor()
-ret = ex.input('data', data)
+ret = ex.input('images', data)
 if ret != 0:
     print('Failed to run input.')
-ret, out = ex.extract('output')
+ret, out = ex.extract('classes')
+ret2, out2 = ex.extract('boxes')
 print(f'inference time: {time.time() - start}s')
 
-for i in range(0, out.h*6, 6):
-    print(f'class: {class_names[int(out[i])-1]}, prob: {out[i+1]}')
+for i in range(80):
+    label = class_names[int(out[i*3])]
+    prob = out[i*3+1]
+    if prob >= 0.25:
+        print(f'{label}:{prob}')
 
 def parser():
     parser = argparse.ArgumentParser(description="YOLOv3 Object Detection")
